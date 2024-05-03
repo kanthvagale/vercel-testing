cd5df4e15d3fe435bfd931741e2e2ae8526e8184
kanthvagale <kanthvagale3@gmail.com>
2024-05-03 22:42:09 +0530

    commenting express.static

--- api/index.js
+++ api/index.js
@@ -11,7 +11,7 @@ require("dotenv").config();
 
 //middleware
 app.use(express.json());
-app.use(express.static("public"));
+// app.use(express.static("public"));
 
 //routes
 app.use("/api/v1/tasks", taskRoutes);
